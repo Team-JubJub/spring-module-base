ffcfcbabed00663a208539af88810331c6a2b518
seasameoil <whdwlgus12@gmail.com>
2023-09-14 16:30:35 +0900

    fix: super builder

--- src/main/java/com/zupzup/untact/model/BaseEntity.java
+++ src/main/java/com/zupzup/untact/model/BaseEntity.java
@@ -2,16 +2,20 @@ package com.zupzup.untact.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-@EntityListeners(AuditingEntityListener.class)
 @Getter @Setter
 @MappedSuperclass
+@SuperBuilder
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
