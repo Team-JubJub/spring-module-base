3a242286748d7570cb93609db12b0ce707e91a3d
seasameoil <whdwlgus12@gmail.com>
2023-11-05 20:00:03 +0900

    fix: module

--- src/main/java/com/zupzup/untact/model/BaseEntity.java
+++ src/main/java/com/zupzup/untact/model/BaseEntity.java
@@ -4,16 +4,12 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.experimental.SuperBuilder;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter @Setter
-@MappedSuperclass
-@SuperBuilder
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
